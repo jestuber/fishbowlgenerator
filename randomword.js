18dd9c5fc8c7d7f79580de32e95c5ba6b7399d39
James Stuber <jestuber@gmail.com>
2018-01-28 22:06:36 -0800

    pulled word lists into its own file, added some words

--- randomword.js
+++ randomword.js
@@ -1,44 +1,3 @@
-var words = [
-    'alligator'
-    ,'ant'
-    ,'bear'
-    ,'bee'
-    ,'bird'
-    ,'camel'
-    ,'cat'
-    ,'cheetah'
-    ,'chicken'
-    ,'chimpanzee'
-    ,'cow'
-    ,'crocodile'
-    ,'deer'
-    ,'dog'
-    ,'dolphin'
-    ,'duck'
-    ,'eagle'
-    ,'elephant'
-    ,'fish'
-    ,'fly'
-    ,'fox'
-    ,'frog'
-    ,'giraffe'
-    ,'goat'
-    ,'goldfish'
-    ,'hamster'
-    ,'hippopotamus'
-    ,'horse'
-    ,'kangaroo'
-    ,'kitten'
-    ,'lion'
-    ,'lobster'
-    ,'monkey'
-    ,'octopus'
-    ,'owl'
-    ,'panda'
-    ,'pig'
-    ,'puppy'
-    ,'rabbit'
-]
 function randomWord(form) {
   $("#cards").html("")
   var numPlayers = form.numPlayers.value;
@@ -46,6 +5,7 @@ function randomWord(form) {
 
   var numWords = numPlayers * numCards
   var result = ""
+  var words = vegetables.concat(vehicles, fruit, clothes, animals)
   let randomWords = _.sample(words, numWords)
   randomWords.forEach((word, index)=>{
     $("#cards").append("<div class='card'>"+word+"</div>")
