cd253d097ab68850182d5ba8f6d51e32847b15e4
James Stuber <jestuber@gmail.com>
2018-01-29 16:27:13 -0800

    adding css template

--- js/randomword.js
+++ js/randomword.js
@@ -1,14 +1,14 @@
 function randomWord(form) {
   $("#cards").html("")
-  var numPlayers = form.numPlayers.value;
-  var numCards = form.numCards.value;
+  var numPlayers = $('#numPlayers').val();
+  var numCards = $('#numCards').val();
 
   var numWords = numPlayers * numCards
   var result = ""
   var words = vegetables.concat(vehicles, fruit, clothes, animals)
   let randomWords = _.sample(words, numWords)
   randomWords.forEach((word, index)=>{
-    $("#cards").append("<div class='card'>"+word+"</div>")
+  $("#cards").append("<div class='card'>"+word+"</div>")
   })
 }
 
