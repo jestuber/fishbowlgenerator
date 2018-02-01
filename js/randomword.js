c8d380b1c437bda9258d3bbc236626bfcafe9aaa
dusty-g <dustygalindo@gmail.com>
2018-01-31 17:15:37 -0800

    added person/place/thing lists

--- js/randomword.js
+++ js/randomword.js
@@ -4,9 +4,19 @@ function randomWord(form) {
   var numCards = $('#numCards').val();
 
   var numWords = numPlayers * numCards
-  var result = ""
-  var words = vegetables.concat(vehicles, fruit, clothes, animals)
-  let randomWords = _.sample(words, numWords)
+  let remainder = 0
+  if(numWords % 3 != 0){
+
+    remainder = numWords % 3 
+  }
+  numWords = Math.floor(numWords / 3)
+
+  let randomThings = _.sample(things, numWords + remainder)
+  
+
+
+  let randomWords = _.shuffle((_.sample(things, numWords + remainder)).concat(_.sample(places, numWords), _.sample(people, numWords)))
+  
   randomWords.forEach((word, index)=>{
   $("#cards").append("<div class='card'>"+word+"</div>")
   })
